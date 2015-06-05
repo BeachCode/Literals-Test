cd342a7be43337069d1748ae485a140e92d9169b
Dustin M <Eleuthera@Dustins-Air.T-mobile.com>
2015-06-04 21:02:52 -0400

    Initial Commit

---
+++ UserDataTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UserDataTest
+//
+//  Created by Dustin M on 6/4/15.
+//  Copyright (c) 2015 Vento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UserDataTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UserDataTest
+//
+//  Created by Dustin M on 6/4/15.
+//  Copyright (c) 2015 Vento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
